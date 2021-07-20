bf320fe84197a00df43bdc61a9174d125124a0ea
Sangmeeeee <goodman1223@naver.com>
2021-07-20 17:50:44 +0900

    JoinContainer 틀

--- frontend/src/container/JoinContainer/JoinContainer.js
+++ frontend/src/container/JoinContainer/JoinContainer.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './JoinContainer.css'
+import { Container, List, Input } from 'semantic-ui-react'
 
 const JoinContainer = () => {
-    return(
-        <div>
-            JoinContainer
-            방 찾기
+    const [ListItems,setListItems] = useState(null)
+
+    const ListItem = (Header, Description) => {
+        return(
+            <List.Item>
+            <List.Content>
+                <List.Header as='a'>{Header}</List.Header>
+                <List.Description as='a'>{Description}</List.Description>
+            </List.Content>
+            </List.Item>
+        )
+    }
+
+    useEffect(() => {
+        setListItems([
+            ListItem('1','1'),
+            ListItem('2','2'),
+            ListItem('3','3'),
+        ])
+        console.log(ListItems)
+    })
+
+
+    return (
+        <div className='JoinContainer'>
+            <div className='JoinSearch'>
+                <Input placeholder='Search...' />
+            </div>
+            <div className='SearchResult'>
+                <List divided relaxed>
+                    {ListItems}
+                </List>
+            </div>
         </div>
     )
 }
