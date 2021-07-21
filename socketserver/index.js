27bf8f9ec42ea03edf1d94fb51214771b43bccbd
Sangmeeeee <goodman1223@naver.com>
2021-07-22 00:26:46 +0900

    node_socket_server

---
+++ socketserver/index.js
@@ -0,0 +1,45 @@
+const http = require('http')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const server = http.createServer(app)
+const io = require('socket.io')(server,
+    {
+        cors : {
+            origin:'*',
+            methods : ["GET","POST"]
+        }
+    })
+
+let rooms = new Array()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+server.listen(9090, () => {
+    console.log('9090 port is open')
+})
+
+app.get('/api/update/room', (req, res) => {
+    console.log(res)
+})
+
+io.on('connection', socket => {
+
+    socket.on('ENTER',(data) => {
+        console.log(data)
+        // socket.join(data.roomId, () => {
+        //     console.log(`${data.ID}가 ${data.roomId}에 참여했습니다.`)
+        //     socket.emit('check')
+        // })
+    })
+
+    socket.join('a', () => {
+        console.log('asd')
+    })
+
+    socket.on('disconnect',() => {
+
+    })
+})
